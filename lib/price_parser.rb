0f73e044e3bb1d6575ded6d47c370cc1357710ee
Victor Ivanov <ivanov-vk@yandex.ru>
2011-01-29 18:11:07 +0300

    added priceHistory to price parsing

--- lib/price_parser.rb
+++ lib/price_parser.rb
@@ -10,30 +10,35 @@ class PriceParser
   end
 
   def create_price(price_attributes)
-    if valid_warehouse_code? price_attributes[:warehouse]
-      find_or_create_by_warehouse_code price_attributes
-    else
-      find_or_create_by_original_description price_attributes
-    end
+    search_term = valid_warehouse_code?(price_attributes[:warehouse]) ?  :warehouse : :original_description
+    create_or_update_price_history(search_term, price_attributes)
   end
 
   private
 
-  def valid_warehouse_code?(code)
-    code && !code.blank?
-  end
-
-  def find_or_create_by_warehouse_code(price_attributes)
-    unless company.prices.find_by_warehouse_code(price_attributes[:warehouse])
-      company.prices.create(price_attributes)
+    def valid_warehouse_code?(code)
+      code && !code.blank?
     end
-  end
 
-  def find_or_create_by_original_description(price_attributes)
-    unless company.prices.find_by_original_description(price_attributes[:original_description])
-      company.prices.create(price_attributes)
+    def create_or_update_price_history(search_term, price_attributes)
+      price = company.prices.where(search_term => price_attributes[search_term])
+      if price.count > 0
+        price.each do |p|
+          last_price_history = p.price_histories.order(:created_at).last
+          if !last_price_history ||
+              (last_price_history.value != price_attributes[:price].to_i && last_price_history.created_at < 10.minutes.ago)  then
+            price_history = p.price_histories.create(:value => price_attributes[:price])
+          end
+        end
+      else
+        company.prices.create(price_attributes)
+      end
     end
-  end
 
+    def find_or_create_by_original_description(price_attributes)
+      unless company.prices.find_by_original_description(price_attributes[:original_description])
+        company.prices.create(price_attributes)
+      end
+    end
 end
 
