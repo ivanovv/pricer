515a4c79dfe94c6c76954dc1f2bc4f1aca0c579b
Victor Ivanov <ivanov-vk@yandex.ru>
2011-03-13 17:31:38 +0300

    Moved back to development env.

--- all.sh
+++ all.sh
@@ -1,7 +1,6 @@
+rake app:citylink > city &
+rake app:fcenter > fcenter &
+rake app:oldi > oldi
+rake app:almer > almer
+rake app:all
 
-RAILS_ENV=production rake app:all > fcenter &
-RAILS_ENV=production rake app:oldi > oldi &
-RAILS_ENV=production rake app:almer > almer
-RAILS_ENV=production rake app:citylink > city
-
-RAILS_ENV=production rake app:all
