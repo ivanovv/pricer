9c253836184287944fd733318b34f2390bea23e5
Victor Ivanov <ivanov-vk@yandex.ru>
2011-04-10 22:50:45 +0400

    kaminary paging with thinking_sphinx

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,15 +5,6 @@ class SearchesController < ApplicationController
       company_param = { :with => {:company_id => company.id} } if params[:company_id]
       @prices = Price.search params[:q], company_param, :per_page => 25, :page => params[:page]
       @items = Item.search(params[:q], :per_page => 25, :page => params[:page])
-      
-      [@prices, @items].each do |a|
-        def a.limit_value
-          per_page
-        end
-        def a.num_pages
-          @total_pages
-        end
-      end
     end
     
     respond_to do |format|
