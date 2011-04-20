d4712b3daf3c7b3ea52cccfc4d8e89f41dc1d828
Victor Ivanov <ivanov-vk@yandex.ru>
2011-04-20 10:11:12 +0400

    Undefined encoding is not no encoding. Need to clarify this later.

--- lib/price_parser_loader.rb
+++ lib/price_parser_loader.rb
@@ -1,7 +1,6 @@
-
 class PriceParserLoader
   def self.load_parser(file)
-    
+
     #require_relative "../lib/#{file}.rb"
     require File.join(File.dirname(__FILE__), "../lib/#{file}.rb")
 
@@ -11,16 +10,19 @@ class PriceParserLoader
       next if excel_parser == XLSParser
       next if !excel_parser.name.downcase.include?(file.downcase)
       next if !defined?(excel_parser::COMPANY_NAME)
-    
+
       string_to_cut_out = excel_parser.superclass == XLSParser ? XLSParser.name : "Parser"
       name = excel_parser.name.sub(string_to_cut_out, '').downcase.to_sym
 
-      desc "Parse #{excel_parser::COMPANY_NAME} price list"      
+      desc "Parse #{excel_parser::COMPANY_NAME} price list"
       task name => :environment do
         file_name = File.expand_path(excel_parser::DEFAULT_FILE_PATH)
-        encoding = excel_parser::DEFAULT_ENCODING if defined?(excel_parser::DEFAULT_ENCODING)
-        excel_parser.parse_price(file_name, encoding)
+        if defined?(excel_parser::DEFAULT_ENCODING)
+          excel_parser.parse_price(file_name, excel_parser::DEFAULT_ENCODING)
+        else
+          excel_parser.parse_price(file_name)
+        end
       end
-    end    
+    end
   end
 end
