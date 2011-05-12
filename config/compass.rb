28f068b2af1fccd5f3c582a218d9f033dbd2916d
Victor Ivanov <ivanov-vk@yandex.ru>
2011-05-13 01:44:59 +0400

    Upgrade to html5 boilerplate 1.0 gem

--- config/compass.rb
+++ config/compass.rb
@@ -1,5 +1,7 @@
 # This configuration file works with both the Compass command line tool and within Rails.
+require 'html5-boilerplate'
 # Require any additional compass plugins here.
+
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
@@ -10,4 +12,13 @@ images_dir = "images"
 javascripts_dir = "public/javascripts"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
-#relative_assets = true
+# relative_assets = true
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass app/stylesheets scss && rm -rf sass && mv scss sass
