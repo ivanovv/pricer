9fd0a786f19d6b0dc603cf36dc45b5df0ae62b78
Victor Ivanov <ivanov-vk@yandex.ru>
2011-06-22 22:10:59 +0400

    Added path to SSL certs.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, '2383780', '5Nc0ZhTFGjefxGf3irWM'
+  provider :vkontakte, '2383780', '5Nc0ZhTFGjefxGf3irWM', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
