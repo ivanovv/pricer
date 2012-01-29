7f7ce0bb807f0318c14b129f6cdf51985eedc7d4
Victor Ivanov <ivanov-vk@yandex.ru>
2012-01-29 13:18:56 +0400

    Commited to the wrong file, should be application.rb, not production.rb.

--- config/application.rb
+++ config/application.rb
@@ -48,6 +48,8 @@ module Pricer
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     #config.generators.stylesheet_engine = :scss
 
     #config.assets.precompile << /(^[^_]|\/[^_])[^\/]*/
