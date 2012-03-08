4f0f7ce18587ac52b48ea2560e78cb81b345e9e7
Victor Ivanov <vic.ivanoff@gmail.com>
2012-03-08 15:17:27 +0400

    Almer price url changed.

--- dl.sh
+++ dl.sh
@@ -2,6 +2,6 @@ cd ~/tmp
 wget -O - -o /dev/null "http://www.fcenter.ru/products/price/price.zip" | funzip > price.html
 wget -O - -o /dev/null "http://www.oldi.ru/price/oldiprr.zip" | funzip > oldiprr.xls
 wget -O - -o /dev/null "http://www.citilink.ru/price/" | funzip > CitilinkPrice_1.xls
-wget -O - -o /dev/null "http://www.almer.ru/files_price/almer.zip" | funzip > almer.xls
+wget -O - -o /dev/null "http://www.almer.ru/price/almer.zip" | funzip > almer.xls
 wget -O - -o /dev/null "http://fast.justcom.ru/prices/JUST_All.zip" | funzip > justcom.xls
 
