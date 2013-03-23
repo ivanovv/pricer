2a2d2ee0f90ccfb3874351bfc97621c0d2f3e0d9
Victor Ivanov <vic.ivanoff@gmail.com>
2013-03-23 19:33:32 +0700

    Configuration Line controller fixes.

--- app/controllers/configuration_lines_controller.rb
+++ app/controllers/configuration_lines_controller.rb
@@ -3,7 +3,8 @@ class ConfigurationLinesController < ApplicationController
   respond_to :html
 
   def index
-    respond_with(@configuration_lines = ConfigurationLine.all)
+    @configuration = ScrapedConfiguration.find params[:scraped_configuration_id]
+    respond_with(@configuration_lines = @configuration.configuration_lines)
   end
 
   def show
@@ -24,6 +25,7 @@ class ConfigurationLinesController < ApplicationController
   end
 
   def edit
+    @configuration = ScrapedConfiguration.find params[:scraped_configuration_id]
     @configuration_line = ConfigurationLine.find(params[:id])
   end
 
